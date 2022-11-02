8aba17d950c4e59dc349b9a3d53a911117423675
Bas Dijkstra <bas@ontestautomation.com>
2022-11-02 14:38:40 +0100

    Fix SA1638 occurrence

--- RestAssured.Net.Tests/Models/Place.cs
+++ RestAssured.Net.Tests/Models/Place.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Location.cs" company="On Test Automation">
+﻿// <copyright file="Place.cs" company="On Test Automation">
 // Copyright 2019 the original author or authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
