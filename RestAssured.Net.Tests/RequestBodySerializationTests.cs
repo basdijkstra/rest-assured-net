fcde102dfad1731aa703fd5b346ff61312b3353d
Bas Dijkstra <bas@ontestautomation.com>
2022-11-10 13:15:46 +0100

    Add example of serializing Dictionary to JSON

--- RestAssured.Net.Tests/RequestBodySerializationTests.cs
+++ RestAssured.Net.Tests/RequestBodySerializationTests.cs
@@ -31,6 +31,7 @@ namespace RestAssuredNet.Tests
     public class RequestBodySerializationTests : TestBase
     {
         private readonly string expectedSerializedJsonRequestBody = "{\"Country\":\"United States\",\"State\":\"California\",\"ZipCode\":90210,\"Places\":[{\"Name\":\"Sun City\",\"Inhabitants\":100000,\"IsCapital\":true},{\"Name\":\"Pleasure Meadow\",\"Inhabitants\":50000,\"IsCapital\":false}]}";
+        private readonly string expectedSerializedObject = "{\"Id\":1,\"Title\":\"My post title\",\"Body\":\"My post body\"}";
         private readonly string xmlBody = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Location xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><Country>United States</Country><State>California</State><ZipCode>90210</ZipCode><Places><Place><Name>Sun City</Name><Inhabitants>100000</Inhabitants><IsCapital>true</IsCapital></Place><Place><Name>Pleasure Meadow</Name><Inhabitants>50000</Inhabitants><IsCapital>false</IsCapital></Place></Places></Location>";
 
         private Location location = new Location();
@@ -83,6 +84,30 @@ namespace RestAssuredNet.Tests
 
         /// <summary>
         /// A test demonstrating RestAssuredNet syntax for serializing
+        /// a Dictionary to JSON and sending itwhen performing an HTTP POST.
+        /// </summary>
+        [Test]
+        public void DictionaryCanBeSerializedToJson()
+        {
+            this.CreateStubForObjectSerialization();
+
+            Dictionary<string, object> post = new Dictionary<string, object>
+            {
+                { "Id", 1 },
+                { "Title", "My post title" },
+                { "Body", "My post body" },
+            };
+
+            Given()
+            .Body(post)
+            .When()
+            .Post("http://localhost:9876/object-serialization")
+            .Then()
+            .StatusCode(201);
+        }
+
+        /// <summary>
+        /// A test demonstrating RestAssuredNet syntax for serializing
         /// and sending an XML request body when performing an HTTP POST.
         /// </summary>
         [Test]
@@ -129,6 +154,17 @@ namespace RestAssuredNet.Tests
         {
             this.Server.Given(Request.Create().WithPath("/json-serialization").UsingPost()
                 .WithBody(new JsonMatcher(this.expectedSerializedJsonRequestBody)))
+                .RespondWith(Response.Create()
+                .WithStatusCode(201));
+        }
+
+        /// <summary>
+        /// Creates the stub response for the object serialization example.
+        /// </summary>
+        private void CreateStubForObjectSerialization()
+        {
+            this.Server.Given(Request.Create().WithPath("/object-serialization").UsingPost()
+                .WithBody(new JsonMatcher(this.expectedSerializedObject)))
                 .RespondWith(Response.Create()
                 .WithStatusCode(201));
         }
